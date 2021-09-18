84e10886d662c75b8b42782b207ebd84a7a55de3
Ravi Prakash <41155449+raviprakashdev@users.noreply.github.com>
2021-09-19 01:52:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import dashboradlogo from "../src/Images/low-bars.png";
+import assessmentslogo from "../src/Images/healthicons_i-exam-multiple-choice-outline.png";
+import candidateslogo from "../src/Images/Group.png";
+import librarylogo from "../src/Images/codicon_library.png";
+import pricinglogo from "../src/Images/icons8_price-tag.png";
+import Dashboard from "./Components/Dashboard";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText,
+} from "reactstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     const [isOpen, setIsOpen] = useState(false);
+
+
+     const toggle = () => setIsOpen(!isOpen);
+  
+    return (
+        <Router>
+        <div>
+          
+                <Navbar expand="md" light className="nav-bg-color py-3">
+                    <NavbarBrand>
+                        
+                    </NavbarBrand>
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse isOpen={isOpen} navbar>
+                        <Nav className="mr-auto " navbar>
+                            <NavItem>
+                                <NavLink
+                                    href="/components/"
+                                    className="nav-text"
+                                >
+                                    <img
+                                        src={dashboradlogo}
+                                        alt="Logo"
+                                        style={{
+                                            verticalAlign: "middle",
+                                            marginTop: "-4px",
+                                        }}
+                                    />
+                                    DASHBOARD
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    href="https://github.com/reactstrap/reactstrap"
+                                    className="nav-text"
+                                >
+                                    <img
+                                        src={assessmentslogo}
+                                        alt="Logo"
+                                        style={{
+                                            verticalAlign: "middle",
+                                            marginTop: "-4px",
+                                        }}
+                                    />
+                                    ASSESSMENTS
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    href="https://github.com/reactstrap/reactstrap"
+                                    className="nav-text activeClick"
+                                >
+                                    <img
+                                        src={candidateslogo}
+                                        alt="Logo"
+                                        style={{
+                                            verticalAlign: "middle",
+                                            marginTop: "-4px",
+                                        }}
+                                    />
+                                    CANDIDATES
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    href="https://github.com/reactstrap/reactstrap"
+                                    className="nav-text"
+                                >
+                                    <img
+                                        src={librarylogo}
+                                        alt="Logo"
+                                        style={{
+                                            verticalAlign: "middle",
+                                            marginTop: "-4px",
+                                        }}
+                                    />
+                                    LIBRARY
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    href="https://github.com/reactstrap/reactstrap"
+                                    className="nav-text"
+                                >
+                                    <img
+                                        src={pricinglogo}
+                                        alt="Logo"
+                                        style={{
+                                            verticalAlign: "middle",
+                                            marginTop: "-4px",
+                                        }}
+                                    />
+                                    PRICING
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                        {/* <NavbarText>Simple Text</NavbarText> */}
+                    </Collapse>
+                </Navbar>
+            </div>
+            <div className="App">
+                <Switch>
+                    {/* <Route path="/signup">
+                        <Signup />
+                    </Route>
+                    <Route path="/login">
+                        <Login />
+                    </Route>
+                    <Route path="/Home">
+                        <Home />
+                    </Route> */}
+                    <Route path="/">
+                        <Dashboard />
+                    </Route>
+                </Switch>
+                {/* <Home /> */}
+                {/* <Navigation />
+                <Signup />
+                <Login /> */}
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/Components/Dashboard.js
@@ -0,0 +1,231 @@
+import React from "react";
+import { Container, Row, Col, Progress } from "reactstrap";
+import thumbUp from "../Images/thumbUp.png";
+import thumpDown from "../Images/thumbDown.png";
+
+const Dashboard = () => {
+    return (
+        <Container fluid="xs">
+            <Row className="h-100 m-0">
+                <Col md="8" className="ml-4">
+                    <Container>
+                        <h1 className="candidate-heading mt-5">
+                            Demo Candidate
+                        </h1>
+
+                        <Row className="mt-3">
+                            <Col md="2" className="candidate-field">
+                                Email:
+                            </Col>
+                            <Col className="candidate-field">
+                                democandidate@gmail.com
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md="2" className="candidate-field">
+                                Phone:
+                            </Col>
+                            <Col className="candidate-field">
+                                +91 9909087654
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md="2" className="candidate-field">
+                                Assessment:
+                            </Col>
+                            <Col className="candidate-field">
+                                Sample assessment(java + javascript + coding)
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md="2" className="candidate-field">
+                                Status:
+                            </Col>
+                            <Col className="candidate-field">
+                                <div
+                                    style={{
+                                        backgroundColor: "#C4C4C4",
+                                        width: "fit-content",
+                                        borderRadius: "5px",
+                                        textDecoration: "underline",
+                                    }}
+                                    className="px-3"
+                                >
+                                    failed
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md="2" className="candidate-field">
+                                Action:
+                            </Col>
+                            <Col className="candidate-field">
+                                {/* <div style={{backgroundColor:"#C4C4C4",width:"fit-content",borderRadius:"5px",textDecoration:"underline"}} className="px-3">failed</div> */}
+                                <div>
+                                    <img
+                                        src={thumbUp}
+                                        alt="Logo"
+                                        style={{
+                                            verticalAlign: "middle",
+                                            marginTop: "-4px",
+                                        }}
+                                        className="mr-4"
+                                    />
+                                    <img
+                                        src={thumpDown}
+                                        alt="Logo"
+                                        style={{
+                                            verticalAlign: "middle",
+                                            marginTop: "-4px",
+                                        }}
+                                    />
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md="2" className="candidate-field">
+                                Created:
+                            </Col>
+                            <Col className="candidate-field">
+                                11/11/11 10:22:22 AM
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md="2" className="candidate-field">
+                                Started:
+                            </Col>
+                            <Col className="candidate-field">
+                                11/11/11 10:22:22 AM
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md="2" className="candidate-field">
+                                Ended:
+                            </Col>
+                            <Col className="candidate-field">
+                                11/11/11 10:22:22 AM
+                            </Col>
+                        </Row>
+                    </Container>
+                    <Container>
+                        <Row>
+                            <Col md="8" className="">
+                                <h1 className="candidate-heading mt-5 mb-5">
+                                    Skill's profile
+                                </h1>
+
+                                <Row className="my-4">
+                                    <Col>
+                                        <div className="d-flex justify-content-between">
+                                            <span>javaScript</span>
+                                            <span>25%</span>
+                                        </div>
+
+                                        <div>
+                                            <Progress value="25" />
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row className="my-4">
+                                    <Col>
+                                        <div className="d-flex justify-content-between">
+                                            <span>javaScript</span>
+                                            <span>25%</span>
+                                        </div>
+
+                                        <div>
+                                            <Progress value="25" />
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row className="my-4">
+                                    <Col>
+                                        <div className="d-flex justify-content-between">
+                                            <span>javaScript</span>
+                                            <span>25%</span>
+                                        </div>
+
+                                        <div>
+                                            <Progress value="25" />
+                                        </div>
+                                    </Col>
+                                </Row>
+                                <Row className="my-4">
+                                    <Col>
+                                        <div className="d-flex justify-content-between">
+                                            <span>javaScript</span>
+                                            <span>25%</span>
+                                        </div>
+
+                                        <div>
+                                            <Progress value="25" />
+                                        </div>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Col>
+                <Col className="">
+                    <div
+                        style={{
+                            height: "320px",
+                            width: "320px",
+                            backgroundColor: "#EFEFEF",
+                            borderRadius: "10px",
+                            margin: "auto",
+                            position: "relative",
+                        }}
+                        className="d-flex align-items-center justify-content-center mt-5"
+                    >
+                        <div
+                            style={{
+                                position: "absolute",
+                                top: "20px",
+                                left: "30px",
+                            }}
+                        >
+                            Score
+                        </div>
+                        <div className="row d-flex justify-content-center mt-100">
+                            <div className="col">
+                                <div className="progressown blue">
+                                    <span className="progressown-left">
+                                        <span className="progressown-bar"></span>{" "}
+                                    </span>
+                                    <span className="progressown-right">
+                                        <span className="progressown-bar"></span>{" "}
+                                    </span>
+                                    <div className="progressown-value">90%</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div
+                            style={{
+                                position: "absolute",
+                                bottom: "20px",
+                                left: "30px",
+                            }}
+                            className="d-flex"
+                        >
+                            <span className="d-flex align-item-center mr-2">
+                                <div
+                                    style={{
+                                        height: "15px",
+                                        width: "15px",
+                                        backgroundColor: "#000000",
+                                        borderRadius: "50%",
+                                    }}
+                                    className="m-auto"
+                                ></div>
+                            </span>
+                            80 out of 150 points
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Dashboard;
